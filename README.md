edaf823b13a537bbf8507530aca4bb1bc86c10cd
Kevin Fowlks <fowlks@msu.edu>
2016-12-09 17:13:49 -0500

    Added comcast code to get usage data

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # aws_alexa_comcast_data_usage_lambda
+
+zip -r ask-alexa_comcast_data_usage_lambda.zip *
--- lambda_function.py
+++ lambda_function.py
@@ -1,13 +1,22 @@
+from __future__ import print_function
 # -*- coding: utf-8 -*-
 """Comcast Internet Usage Alexa Skill
 Author: Kevin Fowlks
 Date:   12/05/2016
-"""
 
+The comcast python code was stolen from https://github.com/lachesis/comcast
+"""
 from ask import alexa
 
+import logging
+import os
+import re
+import requests
 import datetime
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
+logging.getLogger('requests').setLevel(logging.ERROR)
 
 def lambda_handler(request_obj, context=None):
     '''
@@ -74,3 +83,43 @@ def get_internet_usage_intent_handler(request):
     #                              end_session=False, card_obj=card)
 
     return alexa.create_response("You have used %s %s for the month of %s!" % (usageAmt, usageUnits, requestMonth ))
+
+
+def getUsage():
+    session = requests.Session()
+
+    username = os.environ['COMCAST_USERNAME']
+    password = os.environ['COMCAST_PASSWORD']
+
+    logger.debug("Finding req_id for login...")
+    res = session.get('https://login.comcast.net/login?r=comcast.net&s=oauth&continue=https%3A%2F%2Flogin.comcast.net%2Foauth%2Fauthorize%3Fclient_id%3Dmy-account-web%26redirect_uri%3Dhttps%253A%252F%252Fcustomer.xfinity.com%252Foauth%252Fcallback%26response_type%3Dcode%26state%3D%2523%252Fdevices%26response%3D1&client_id=my-account-web')
+    assert res.status_code == 200
+    m = re.search(r'<input type="hidden" name="reqId" value="(.*?)">', res.text)
+    req_id = m.group(1)
+    logger.debug("Found req_id = %r", req_id)
+
+    data = {
+        'user': username,
+        'passwd': password,
+        'reqId': req_id,
+        'deviceAuthn': 'false',
+        's': 'oauth',
+        'forceAuthn': '0',
+        'r': 'comcast.net',
+        'ipAddrAuthn': 'false',
+        'continue': 'https://login.comcast.net/oauth/authorize?client_id=my-account-web&redirect_uri=https%3A%2F%2Fcustomer.xfinity.com%2Foauth%2Fcallback&response_type=code&state=%23%2Fdevices&response=1',
+        'passive': 'false',
+        'client_id': 'my-account-web',
+        'lang': 'en',
+    }
+
+    logger.debug("Posting to login...")
+    res = session.post('https://login.comcast.net/login', data=data)
+    assert res.status_code == 200
+
+    logger.debug("Fetching internet usage AJAX...")
+    res = session.get('https://customer.xfinity.com/apis/services/internet/usage')
+    assert res.status_code == 200
+
+    return res.text
+
