86e66e4051456093ce451fce579cd35bd64f8d45
Kevin Fowlks <fowlk1kd@gmail.com>
2016-12-11 21:34:40 -0500

    update comments

--- lambda_function.py
+++ lambda_function.py
@@ -6,191 +6,6 @@ Date:   12/05/2016
 
 The comcast python code was stolen from https://github.com/lachesis/comcast
 
-
-{
-   "courtesyUsed":0,
-   "courtesyRemaining":2,
-   "courtesyAllowed":2,
-   "inPaidOverage":false,
-   "usageMonths":[
-      {
-         "policyName":"Xfinity Data Plan",
-         "startDate":"06/01/2016",
-         "endDate":"06/30/2016",
-         "homeUsage":1759.0,
-         "allowableUsage":250,
-         "unitOfMeasure":"GB",
-         "devices":[
-
-         ],
-         "additionalBlocksUsed":0.0,
-         "additionalCostPerBlock":0.0,
-         "additionalUnitsPerBlock":null,
-         "additionalIncluded":0.0,
-         "additionalUsed":0.0,
-         "additionalPercentUsed":0.0,
-         "additionalRemaining":0.0,
-         "billableOverage":0.0,
-         "overageCharges":0.0,
-         "overageUsed":0.0,
-         "currentCreditAmount":0,
-         "maxCreditAmount":0,
-         "policy":"suspended"
-      },
-      {
-         "policyName":"Xfinity Data Plan",
-         "startDate":"07/01/2016",
-         "endDate":"07/31/2016",
-         "homeUsage":2037.0,
-         "allowableUsage":250,
-         "unitOfMeasure":"GB",
-         "devices":[
-
-         ],
-         "additionalBlocksUsed":0.0,
-         "additionalCostPerBlock":0.0,
-         "additionalUnitsPerBlock":null,
-         "additionalIncluded":0.0,
-         "additionalUsed":0.0,
-         "additionalPercentUsed":0.0,
-         "additionalRemaining":0.0,
-         "billableOverage":0.0,
-         "overageCharges":0.0,
-         "overageUsed":0.0,
-         "currentCreditAmount":0,
-         "maxCreditAmount":0,
-         "policy":"suspended"
-      },
-      {
-         "policyName":"Xfinity Data Plan",
-         "startDate":"08/01/2016",
-         "endDate":"08/31/2016",
-         "homeUsage":1464.0,
-         "allowableUsage":250,
-         "unitOfMeasure":"GB",
-         "devices":[
-
-         ],
-         "additionalBlocksUsed":0.0,
-         "additionalCostPerBlock":0.0,
-         "additionalUnitsPerBlock":null,
-         "additionalIncluded":0.0,
-         "additionalUsed":0.0,
-         "additionalPercentUsed":0.0,
-         "additionalRemaining":0.0,
-         "billableOverage":0.0,
-         "overageCharges":0.0,
-         "overageUsed":0.0,
-         "currentCreditAmount":0,
-         "maxCreditAmount":0,
-         "policy":"suspended"
-      },
-      {
-         "policyName":"Xfinity Data Plan",
-         "startDate":"09/01/2016",
-         "endDate":"09/30/2016",
-         "homeUsage":704.0,
-         "allowableUsage":250,
-         "unitOfMeasure":"GB",
-         "devices":[
-
-         ],
-         "additionalBlocksUsed":0.0,
-         "additionalCostPerBlock":0.0,
-         "additionalUnitsPerBlock":null,
-         "additionalIncluded":0.0,
-         "additionalUsed":0.0,
-         "additionalPercentUsed":0.0,
-         "additionalRemaining":0.0,
-         "billableOverage":0.0,
-         "overageCharges":0.0,
-         "overageUsed":0.0,
-         "currentCreditAmount":0,
-         "maxCreditAmount":0,
-         "policy":"suspended"
-      },
-      {
-         "policyName":"Xfinity Data Plan",
-         "startDate":"10/01/2016",
-         "endDate":"10/31/2016",
-         "homeUsage":768.0,
-         "allowableUsage":250,
-         "unitOfMeasure":"GB",
-         "devices":[
-
-         ],
-         "additionalBlocksUsed":0.0,
-         "additionalCostPerBlock":0.0,
-         "additionalUnitsPerBlock":null,
-         "additionalIncluded":0.0,
-         "additionalUsed":0.0,
-         "additionalPercentUsed":0.0,
-         "additionalRemaining":0.0,
-         "billableOverage":0.0,
-         "overageCharges":0.0,
-         "overageUsed":0.0,
-         "currentCreditAmount":0,
-         "maxCreditAmount":0,
-         "policy":"suspended"
-      },
-      {
-         "policyName":"1 Terabyte Data Plan",
-         "startDate":"11/01/2016",
-         "endDate":"11/30/2016",
-         "homeUsage":866.0,
-         "allowableUsage":1024.0,
-         "unitOfMeasure":"GB",
-         "devices":[
-            {
-               "id":"78:96:84:FA:2F:F0",
-               "usage":866.0
-            }
-         ],
-         "additionalBlocksUsed":0.0,
-         "additionalCostPerBlock":10.0,
-         "additionalUnitsPerBlock":50.0,
-         "additionalIncluded":0.0,
-         "additionalUsed":0.0,
-         "additionalPercentUsed":0.0,
-         "additionalRemaining":0.0,
-         "billableOverage":0.0,
-         "overageCharges":0.0,
-         "overageUsed":0.0,
-         "currentCreditAmount":0,
-         "maxCreditAmount":0,
-         "policy":"limited"
-      },
-      {
-         "policyName":"1 Terabyte Data Plan",
-         "startDate":"12/01/2016",
-         "endDate":"12/31/2016",
-         "homeUsage":268.0,
-         "allowableUsage":1024.0,
-         "unitOfMeasure":"GB",
-         "devices":[
-            {
-               "id":"78:96:84:FA:2F:F0",
-               "usage":268.0
-            }
-         ],
-         "additionalBlocksUsed":0.0,
-         "additionalCostPerBlock":10.0,
-         "additionalUnitsPerBlock":50.0,
-         "additionalIncluded":0.0,
-         "additionalUsed":0.0,
-         "additionalPercentUsed":0.0,
-         "additionalRemaining":0.0,
-         "billableOverage":0.0,
-         "overageCharges":0.0,
-         "overageUsed":0.0,
-         "currentCreditAmount":0,
-         "maxCreditAmount":0,
-         "policy":"limited"
-      }
-   ]
-}
-
-
 """
 from ask import alexa
 #from dateutil.parser import parse
@@ -229,28 +44,36 @@ def lambda_handler(request_obj, context=None):
     Otherwise your server can hit this code as long as you remember that the
     input 'request_obj' is JSON request converted into a nested python object.
     '''
-
-    metadata = {'user_name' : 'SomeRandomDude'} # add your own metadata to the request using key value pairs
     
-    ''' inject user relevant metadata into the request if you want to, here.    
-    e.g. Something like : 
-    ... metadata = {'user_name' : some_database.query_user_name(request.get_user_id())}
 
-    Then in the handler function you can do something like -
-    ... return alexa.create_response('Hello there {}!'.format(request.metadata['user_name']))
-    '''
+    metadata  = request.metadata
+
+    if 'usageMonths' not in metadata:
+        jsonStr = getUsage()
+
+        if jsonStr == None:
+            card = alexa.create_card(title="GetInternetUsage activated", subtitle=None,
+                                    content="asked alexa to get internet usage")
+            return alexa.create_response("Sorry unable to retrieve comcast account information", end_session=True, card_obj=card)
+
+        jsonObj = json.loads(jsonStr)
+
+        usageMonths = jsonObj['usageMonths']
+
+        metadata = {'usageMonths' : usageMonths } # add your own metadata to the request using key value pairs
+
     return alexa.route_request(request_obj, metadata)
 
 
 @alexa.default_handler()
 def default_handler(request):
     """ The default handler gets invoked if no handler is set for a request """
-    return alexa.create_response(message="Just ask")
+    return alexa.create_response(message="Just ask").with_card('What is my current internet usage')
 
 
 @alexa.request_handler("LaunchRequest")
 def launch_request_handler(request):
-    return alexa.create_response(message="Hello Welcome to Comcast Internet Usage!")
+    return alexa.create_response(message="Hello Welcome to Internet Usage!")
 
 
 @alexa.request_handler("SessionEndedRequest")
@@ -260,48 +83,33 @@ def session_ended_request_handler(request):
 
 @alexa.intent_handler('GetInternetUsage')
 def get_internet_usage_intent_handler(request):
-
-
-    jsonStr = getUsage()
-    jsonObj = json.loads(jsonStr)
-    usageMonths = jsonObj['usageMonths']
-
-    myUsageMonths = {}
     
-    requestUserMonthByName = ""
+    myUsageMonths = {}
+
+    usageMonths = request.metadata['usageMonths']
+    
+    for usageMonth in usageMonths:
+        monthName = calendar.month_name[int(usageMonth['startDate'][:2])]
+        myUsageMonths[monthName] = MyUsage( monthName, usageMonth['unitOfMeasure'], usageMonth['homeUsage'], usageMonth['allowableUsage'])
+    
     requestMonth = request.slots["Date"]
 
     if requestMonth == None:
-        currentDT = datetime.datetime.now()    
+        currentDT = datetime.datetime.now()  # If users request does not have a date lets default to the current date 
         requestUserMonthByName = currentDT.strftime("%B") # Get Month by name e.g.    
     else:    
         requestUserMonthByName = calendar.month_name[int(requestMonth[5] + requestMonth[6])]
 
-    for usageMonth in usageMonths:
-        monthName = calendar.month_name[int(usageMonth['startDate'][:2])]
-        myUsageMonths[monthName] = MyUsage( monthName, usageMonth['unitOfMeasure'], usageMonth['homeUsage'], usageMonth['allowableUsage'])
-
     if requestUserMonthByName not in myUsageMonths:
         card = alexa.create_card(title="GetInternetUsage activated", subtitle=None,
                                  content="asked alexa to get internet usage using {}".format(requestMonth))
-        return alexa.create_response("Sorry no usage data exist for the requested month  {}".format(requestMonth), end_session=False, card_obj=card)
-
-    return alexa.create_response("You have used %s%s out of %s%s for the month of %s!" % (myUsageMonths[requestUserMonthByName].homeUsage, 
-                                                                              myUsageMonths[requestUserMonthByName].unitOfMeasure, 
-                                                                              myUsageMonths[requestUserMonthByName].allowableUsage,
-                                                                              myUsageMonths[requestUserMonthByName].unitOfMeasure,
-                                                                              myUsageMonths[requestUserMonthByName].monthLongName ))
-  
-    #for element in jsonObj['usageMonths']:
-    #    print element.startDate
-    #    print element.homeUsage
-    #"startDate":"12/01/2016",
-    #         "endDate":"12/31/2016",
-    #         "homeUsage":268.0,
-    #        "allowableUsage":1024.0,
-    # 
-    #     return alexa.create_response("Could not find an ingredient!")
-
+        return alexa.create_response("Sorry no usage data exist for the requested month {}".format(requestMonth), end_session=False, card_obj=card)
+    else:
+        return alexa.create_response("You have used %s%s out of %s%s for the month of %s!" % (myUsageMonths[requestUserMonthByName].homeUsage, 
+                                                                                              myUsageMonths[requestUserMonthByName].unitOfMeasure, 
+                                                                                              myUsageMonths[requestUserMonthByName].allowableUsage,
+                                                                                              myUsageMonths[requestUserMonthByName].unitOfMeasure,
+                                                                                              myUsageMonths[requestUserMonthByName].monthLongName ))
 def getUsage():
     session = requests.Session()
 
@@ -310,7 +118,10 @@ def getUsage():
 
     logger.debug("Finding req_id for login...")
     res = session.get('https://login.comcast.net/login?r=comcast.net&s=oauth&continue=https%3A%2F%2Flogin.comcast.net%2Foauth%2Fauthorize%3Fclient_id%3Dmy-account-web%26redirect_uri%3Dhttps%253A%252F%252Fcustomer.xfinity.com%252Foauth%252Fcallback%26response_type%3Dcode%26state%3D%2523%252Fdevices%26response%3D1&client_id=my-account-web')
-    assert res.status_code == 200
+
+    if res.status_code != 200:
+        return None
+
     m = re.search(r'<input type="hidden" name="reqId" value="(.*?)">', res.text)
     req_id = m.group(1)
     logger.debug("Found req_id = %r", req_id)
@@ -332,51 +143,14 @@ def getUsage():
 
     logger.debug("Posting to login...")
     res = session.post('https://login.comcast.net/login', data=data)
-    assert res.status_code == 200
+    
+    if res.status_code != 200:
+        return None
 
     logger.debug("Fetching internet usage AJAX...")
     res = session.get('https://customer.xfinity.com/apis/services/internet/usage')
-    assert res.status_code == 200
+    
+    if res.status_code != 200:
+        return None
 
     return res.text
-
-
-
-# jsonStr = getUsage()
-# jsonObj = json.loads(jsonStr)
-# usageMonths = jsonObj['usageMonths']
-
-# myUsageMonths = {}
-
-# requestMonth = '2016-09-12'
-# print ('Hello')
-# print (  requestMonth[5] + requestMonth[6] )
-
-# requestUserMonthByName = ""
-
-# if requestMonth == None:
-#     currentDT = datetime.datetime.now()    
-#     requestUserMonthByName = currentDT.strftime("%B") # Get Month by name e.g.    
-# else:    
-#     requestUserMonthByName = calendar.month_name[int(requestMonth[5] + requestMonth[6])]
-
-# jsonStr = getUsage()
-# jsonObj = json.loads(jsonStr)
-# usageMonths   = jsonObj['usageMonths']
-
-# print (len(usageMonths))
-
-# for usageMonth in usageMonths:
-#     monthName = calendar.month_name[int(usageMonth['startDate'][:2])]
-#     myUsageMonths[monthName] = MyUsage( monthName, usageMonth['unitOfMeasure'], usageMonth['homeUsage'], usageMonth['allowableUsage'])
-
-# print (requestUserMonthByName)
-# print (myUsageMonths[requestUserMonthByName].homeUsage)
-# print (myUsageMonths[requestUserMonthByName].unitOfMeasure)
-# print (myUsageMonths[requestUserMonthByName].allowableUsage)
-
-# print ("You have used %s %s out of %s %s for the month of %s!" % (myUsageMonths[requestUserMonthByName].homeUsage, 
-#                                                                 myUsageMonths[requestUserMonthByName].unitOfMeasure, 
-#                                                                 myUsageMonths[requestUserMonthByName].allowableUsage,
-#                                                                 myUsageMonths[requestUserMonthByName].unitOfMeasure,
-#                                                                 myUsageMonths[requestUserMonthByName].monthLongName ))
